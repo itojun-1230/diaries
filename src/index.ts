8ae84f17be0ce9c4ce736e368f697b782180ab99
itojun-1230 <juntou1230@gmail.com>
2025-03-09 11:55:06 +0900

    feat: 日記移動機能を追加
    日記ファイルを作成日に基づいて年・月フォルダに自動的に移動する moveDiary 関数を実装。
    グローバルスコープに関数を公開し、プロジェクトフォルダユーティリティを活用して柔軟なフォルダ管理を実現。

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { createDiary } from "./feature/createDiary";
+import { moveDiary } from "./feature/moveDiary";
 
 (global as any).createDiary = createDiary;
+(global as any).moveDiary = moveDiary;

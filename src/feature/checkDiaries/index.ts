2aeaaeb0f373cff7b9b6587e0923fabfb3df2f65
itojun-1230 <juntou1230@gmail.com>
2025-03-09 13:43:39 +0900

    feat: 複数の日記を確認し、更新されたものを自動的に移動する機能を追加
    複数の日記ファイルを一括で確認し、更新されているものを自動的に移動する checkDiaries 関数を実装。index.ts に新しい関数をグローバルスコープに追加し、日記管理プロセスを拡張。

---
+++ src/feature/checkDiaries/index.ts
@@ -0,0 +1,23 @@
+import { projectFolder } from "@/utils/projectFolder";
+import { isDiaryUpdated } from "@/funcs/isDiaryUpdated";
+import { moveDiary } from "../moveDiary";
+
+/**
+ * 日記を確認し、更新されている場合は移動する
+ */
+export const checkDiaries = () => {
+    const currentFolder = projectFolder();
+    const diaries = currentFolder.getFiles();
+    while(diaries.hasNext()) {
+        const diary = diaries.next();
+        const fileName = diary.getName();
+        // ファイル名が日記 yyyy-mm-dd.md の形式であるか確認
+        if(!fileName.match(/^日記 \d{4}-\d{2}-\d{2}\.md$/)) {
+            continue;
+        }
+
+        if(isDiaryUpdated(diary)) {
+            moveDiary(diary);
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { checkTodayDiary } from "./feature/checkTodayDiary";
+import { checkDiaries } from "./feature/checkDiaries";
 
 (global as any).checkTodayDiary = checkTodayDiary;
+(global as any).checkDiaries = checkDiaries;

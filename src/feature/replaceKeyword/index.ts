bb40993a4d2b6bf701893f21a9ed56e3d8ccd9dc
itojun-1230 <juntou1230@gmail.com>
2025-03-10 12:06:36 +0900

    feat: キーワード置換処理を関数に分割し、重複置換を防止
    キーワード置換のロジックを replaceKeywordWithBrackets と replaceKeywords 関数に分割。
    既に [[keyword]] 形式のキーワードを重複して置換しないよう、事前チェックを追加。
    コードの可読性と処理の柔軟性を向上。

--- src/feature/replaceKeyword/index.ts
+++ src/feature/replaceKeyword/index.ts
@@ -1,6 +1,39 @@
 import { getKeywordsFromFile } from "../getKeywordsFromFile";
 
 /**
+ * キーワードを[[keyword]]の形式に置換する
+ * すでに[[]]で囲まれているキーワードはスキップする
+ * @param content 置換対象のテキスト
+ * @param keyword 置換するキーワード
+ * @returns 置換後のテキスト
+ */
+const replaceKeywordWithBrackets = (content: string, keyword: string): string => {
+    // すでに[[keyword]]の形式になっているものを検出する正規表現
+    const wrappedPattern = new RegExp(`\\[\\[${keyword}\\]\\]`);
+
+    // すでに[[]]で囲まれている場合はスキップ
+    if (wrappedPattern.test(content)) {
+        return content;
+    }
+
+    return content.replace(keyword, `[[${keyword}]]`);
+};
+
+/**
+ * テキスト内のキーワードを[[keyword]]の形式に置換する
+ * @param content 置換対象のテキスト
+ * @param keywords 置換するキーワードの配列
+ * @returns 置換後のテキスト
+ */
+export const replaceKeywords = (content: string, keywords: string[]): string => {
+    let result = content;
+    for (const keyword of keywords) {
+        result = replaceKeywordWithBrackets(result, keyword);
+    }
+    return result;
+};
+
+/**
  * 日記ファイルの内容をキーワードで置換する
  * @param {GoogleAppsScript.Drive.File} diaryFile - 日記ファイル
  * @returns {GoogleAppsScript.Drive.File} 置換後の日記ファイル
@@ -9,7 +42,7 @@ export const replaceKeyword = async (diaryFile: GoogleAppsScript.Drive.File) => 
     const keywords = (await getKeywordsFromFile(diaryFile)).sort((a, b) => b.length - a.length);
     let content = diaryFile.getBlob().getDataAsString();
     for (const keyword of keywords) {
-        content = content.replace(keyword, `[[${keyword}]]`);
+        content = replaceKeywordWithBrackets(content, keyword);
     }
     diaryFile.setContent(content);
 }
